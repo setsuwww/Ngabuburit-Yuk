0878dcf855cc8742950bbbfad57cb05080c5b2b6
setsuwww <setsuprojectonly@gmail.com>
2026-03-09 11:38:48 +0700

    Admin dashboard

---
+++ app/layouts/admin-dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="bg-white">
+        <h1 class="bg-black text-red-500">Admin | Dashboard</h1>
+        <slot />
+    </div>
+</template>
---
+++ app/pages/admin/dashboard.vue
@@ -0,0 +1,14 @@
+<script setup>
+definePageMeta({
+  layout: "admin-dashboard"
+});
+useHead({
+  title: "Admin | Dashboard"
+})
+</script>
+
+<template>
+  <section>
+    <p class="text-emerald-500">This page will be displayed at the /admin/dashboard</p>
+  </section>
+</template>
