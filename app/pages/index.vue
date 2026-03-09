45938105fd2caf4cafa7284ba1c78bf89d57b645
setsuwww <setsuprojectonly@gmail.com>
2026-03-09 11:33:06 +0700

    Login

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,3 @@
-<template>
-  <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
-  </div>
-</template>
+<script setup>
+  await navigateTo('/auth/login')
+</script>
